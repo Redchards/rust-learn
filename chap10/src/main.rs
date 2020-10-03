060a1ad11b7fec96060893c4852beb774639186d
Redchards <urien.loic.cours@gmail.com>
2020-10-04 00:46:16 +0200

    completed the section about traits

--- chap10/src/main.rs
+++ chap10/src/main.rs
@@ -1,4 +1,7 @@
 use std::cmp;
+use std::cmp::PartialOrd;
+use std::fmt;
+use std::fmt::{Display, Formatter};
 
 fn main() {
     // Now let's talk about generics. Yay!
@@ -67,7 +70,89 @@ fn main() {
 
     println!("The largest element in {:?} is {}", num_list, max_elem(&num_list));
     println!("The largest element in {:?} is {}", char_list, max_elem(&char_list));
-}
+
+    let integer_point = Point { x: 5, y: 10 };
+    let float_point = Point { x: 1.0, y: 4.0 };
+
+    // Of course T is a unique type, we can't mix types together
+    // The following line would not compile
+    // let wrong_point = Point { x: 1, y: 4.0 };
+
+    println!("{:?}", integer_point);
+    println!("{:?}", float_point);
+    println!("{}", integer_point.x());
+    println!("{}", float_point.x());
+
+    println!("{:?}", float_point.mixup(integer_point));
+
+    let right_point = MixPoint { x: 1, y: 4.0 };
+    let float_point = MixPoint { x: 1.0, y: 4.0 };
+
+    println!("{:?}", right_point.mixup(float_point));
+    
+    let p1 = Point { x: 1.0, y: 4.0 };
+    let p2 = Point { x: 2.0, y: 7.35 };
+    println!("Distance between {:?} and {:?} = {}", p1, p2, p1.distance_fron(&p2));
+
+    // Example traits
+    let tweet = Tweet {
+        username: String::from("mother_horse_eyes"),
+        content: String::from(
+            "what awaits us beyond the curtains?"
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    // Slightly politicized example
+    let article = NewsArticle {
+        headline: String::from("Orange man bad"),
+        location: String::from("The MSM"),
+        author: String::from("Anton McCuck"),
+        content: String::from("Orange man got COVID, ahah, orange man dead"),
+    };
+
+    println!("1 new tweet: {}", article.summarize());
+
+    // This one everyone will agree with, most likely anyway
+    let tabloid_article = TabloidNewsArticle {
+        headline: String::from("THIS SINGLE MOM OF 3 FOUND A REVOLUTIONARY WAY TO LOOK 10 YEARS YOUNGER, DOCTORS HATE HER"),
+        location: String::from("BuzzBizzFeedzies"),
+        author: String::from("CoolLife69"),
+        content: String::from("Amazing trick, but first a shitload of ads!!!"),
+    };
+
+    println!("1 new tweet: {}", tabloid_article.summarize());
+
+    notify(&tweet);
+    notify2(&tweet);
+    dual_notify(&tweet, &article);
+    // Can't do the following:
+    // dual_notify_bound(&tweet, &article);
+
+    dual_notify_bound(&tweet, &tweet);
+
+    notify_spam(&tabloid_article);
+    notify_spam2(&tabloid_article);
+    notify_spam3(&tabloid_article);
+
+    returns_summarizable();
+
+    let p1 = Pair::new(3, 5);
+    p1.cmp();
+    p1.cmp_display();
+
+    // The following won't compile because the type Tweet does not implement the proper traits
+    // let p2 = Pair { first: &tweet, second: &tweet };
+    // p2.cmp();
+    // p2.cmp_display();
+
+    // Now let's move onto Lifetimes
+    // This time, instead of wanting to know if a type has the correct behavior, we want to determine the lifetime of a particular resource, which
+    // is the scope in which a reference to it is valid
+}       
 
 // VERBOTTEN!!!
 // fn max_element(x: &[i32]) -> i32 {...}
@@ -126,3 +211,239 @@ fn max_elem<T:cmp::PartialOrd>(l: &[T]) -> &T {  // Returning a ref here because
 
    curr_max
 }
+
+// Using generics in structs is very straightforward as well
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+// And in method definitions
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        return &self.x;
+    }
+}
+
+
+#[derive(Debug)]
+struct MixPoint<T, U> {
+    x: T,
+    y: U
+}
+
+impl<T1, U1> MixPoint<T1, U1> {
+    fn mixup<T2, U2>(self, other: MixPoint<T2, U2>) -> MixPoint<T1, U2> {
+        MixPoint {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+impl<T1> Point<T1> {
+    fn mixup<T2>(self, other: Point<T2>) -> MixPoint<T1, T2> {
+        MixPoint {
+            x: self.x,
+            y: other.y,
+        }
+
+    }
+}
+
+// Implementing a method only on f64
+impl Point<f64> {
+    fn distance_fron(&self, other: &Point<f64>) -> f64 {
+        let x = other.x - self.x;
+        let y = other.y - self.y;
+
+        (x*x + y*y).sqrt()
+    }
+}
+
+// Here's a little trait
+pub trait Summarizable {
+    fn summarize(&self) -> String;
+}
+
+// As we can see, implementing a trait on a type is fairly straightforward
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summarizable for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+// We can also provide a default implementation
+pub trait SummarizableWithDefault {
+    fn summarize(&self) -> String {
+        String::from("Read more...")
+    }
+}
+
+pub struct TabloidNewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl SummarizableWithDefault for TabloidNewsArticle {}
+impl Display for TabloidNewsArticle {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}...", &self.content[..20])
+    }
+}
+
+// We can simply take a trait as parameter
+fn notify(item: &impl Summarizable)  {
+    println!("Breaking new! {}", item.summarize());
+}
+
+// The syntax above is a shorthand for the following one
+fn notify2<T: Summarizable>(item: &T) {
+    println!("Breaking new! {}", item.summarize());
+}
+
+// The "trait bound syntax" is not useless though, consider the following case
+fn dual_notify(item1: &impl Summarizable, item2: &impl Summarizable) {
+    println!("Breaking new! {}", item1.summarize());
+    println!("Breaking new! {}", item2.summarize());
+}
+
+// Using this syntax, we can't force the types to be the same though, we have to use the trait bound syntax
+fn dual_notify_bound<T: Summarizable>(item1: &T, item2: &T) {
+    println!("Breaking new! {}", item1.summarize());
+    println!("Breaking new! {}", item2.summarize());
+}
+
+// We can also specify multiple traits using the + operator
+fn notify_spam(item: &(impl SummarizableWithDefault + Display)) {
+    println!("Spam found! {}", item.summarize());
+    println!("Peek content => {}", item);
+}
+
+// Which is of course equivalent to the following syntax
+fn notify_spam2<T: SummarizableWithDefault + Display>(item: &T) {
+    println!("Spam found! {}", item.summarize());
+    println!("Peek content => {}", item);
+}
+
+// We can also use the handy 'where' syntax
+fn notify_spam3<T>(item: &T)
+    where T: SummarizableWithDefault + Display
+{
+    
+    println!("Spam found! {}", item.summarize());
+    println!("Peek content => {}", item);
+}
+
+// We can also write a function that returns a type that implements a traits, that being said, we can't return
+// either one type or another, so the following function is correct for instance: 
+fn returns_summarizable() -> impl Summarizable {
+    Tweet {
+        username: String::from("ProbablySomethingQuirky"),
+        content: String::from(
+            "Something very witty and imaginative, because twitter users are big brained"
+        ),
+        reply: false,
+        retweet: false,
+    }
+}
+
+// But this one is not allowed, giving us an error 'expected struct Tweet, found struct NewsArticle
+// if and else have incompatible types
+// Quite exlicit honestly
+/* fn returns_summarizable_switch(switch: bool) -> impl Summarizable {
+    if switch {
+        Tweet {
+            username: String::from("ProbablySomethingQuirky"),
+            content: String::from(
+                "Something very witty and imaginative, because twitter users are big brained"
+            ),
+            reply: false,
+            retweet: false,
+        }
+    }
+    else {
+        NewsArticle {
+            headline: String::from("Breaking News : very interesting, you want to know!!!"),
+            location: String::from("Some big news outlet"),
+            author: String::from("Big McChungus"),
+            content: String::from("Blah blah blah, the world is ending, blah blah blah"),
+        }
+    }
+} */
+
+// Traits also allow us to conditionally implement methods
+// Let's consider the following struct
+struct Pair<T> {
+    first: T,
+    second: T,
+}
+
+enum PairElementComparisonResult {
+    FirstGtSecond,
+    SecondGtFirst,
+    BothEqual
+}
+
+// The new method is implemented for any type
+impl<T> Pair<T> {
+    fn new(first: T, second: T) -> Self {
+        Self { first, second }
+    }
+}
+
+// The cmp method is implemented only for T bounded with PartialOrd
+impl<T: PartialOrd> Pair<T> {
+    fn cmp(&self) -> PairElementComparisonResult {
+        if self.first > self.second {
+            PairElementComparisonResult::FirstGtSecond
+        }
+        else if self.first < self.second {
+            PairElementComparisonResult::SecondGtFirst
+        }
+        else {
+            PairElementComparisonResult::BothEqual
+        }
+    }
+} 
+
+// The cmp_display method is implemented only for T bounded with PartialOrd and Display
+impl<T: PartialOrd + Display> Pair<T> {
+    fn cmp_display(&self) {
+        match self.cmp() {
+            PairElementComparisonResult::FirstGtSecond => println!("The largest member is {}", self.first),
+            PairElementComparisonResult::SecondGtFirst => println!("The largest member is {}", self.second),
+            PairElementComparisonResult::BothEqual => println!("Both members are equal"),
+        }
+    }
+}
+
+// We can also conditionally implement a trait for any type that implement another trait, which is neat
+// impl<T: Display> ToString for T {...}
+// The standard library actually defined this trait, so for any type that implements Display, we can us the
+// .to_string method
+// let s = 3.to_string();
+// We call it a blanket implementation, they appear in the documentation for the trait in the "Implementors" section
