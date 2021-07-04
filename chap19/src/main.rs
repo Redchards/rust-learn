4aa4d9ce117f06a75bdd7ad7e6355d77c9627e8c
Redchards <urien.loic.cours@gmail.com>
2021-07-04 20:44:04 +0200

    keep on working on chap19

--- chap19/src/main.rs
+++ chap19/src/main.rs
@@ -1,3 +1,6 @@
+use std::slice;
+use std::ops::Add;
+
 fn main() {
     // Unsafe rust gives us the following 5 superpowers :
     // * Dereference raw pointers
@@ -36,5 +39,166 @@ fn main() {
     unsafe {
         println!("*r1 : {}", *r1);
         println!("*r2 : {}", *r2);
+    }
+
+    unsafe {
+        dangerous();
+    }
+
+    // We obviously can't call the "dangerous" function outside of an unsafe block
+    // This is inconvenient, so very often we want to create a safe abstraction over an unsafe block of code
+    // Let's look at the "split_at_mut" method :
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = split_at_mut(r, 3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+
+    // Unsafe allows us to do things like accessing arbitrary memory locations, which can be dangerous
+    // The following code would crash upon using the "slice"
+
+    let addr = 0x012345usize;
+    let r = addr as *mut i32;
+
+    // We're creating a slice that is 1000 items long from an arbitrary memory address
+    let _slice: &[i32] = unsafe { slice::from_raw_parts_mut(r, 1000) };
+
+    // We can mark functions as extern "C" to interact with C libraries
+    // C functions can only be called within an unsafe block
+    // The other way around, but can tag a function #[no_mangle] and extern "C" to have C interact with Rust
+
+    // Global mutable variables are, by nature, unsafe, as they could very easily lead to data races, we can't access
+    // them without an "unsafe" block
+
+    // Operator overloading through traits
+    let p1 = Point { x: 1, y: 0 };
+    let p2 = Point { x: 2, y: 1 };
+
+    println!("p1 + p2 = {:?}", p1 + p2);
+
+    // It must be noted that the Add trait has a default generic parameter :
+    // trait Add<Rhs=Self> {
+    //     type Output;
+
+    //     fn add(self, rhs: Rhs) -> Self::Output;
+    // }
+
+    let d1 = Millimeters(250);
+    let d2 = Meters(2);
+    println!("d1 + d2 = {:?}", d1 + d2);
+
+    // We can also disambiguate function calls with the same name implemented by different traits
+
+    let dog = Doggo;
+
+    Small::bark(&dog);
+    Big::bark(&dog);
+    Doggo::bark(&dog);
+    dog.bark();
+
+    // The two last ones are strictly equivalent
+}
+
+unsafe fn dangerous() {}
+
+// For instance, we can't write the split_at_mut function this way
+// cannot borrow `*slice` as mutable more than once at a time
+/* fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+
+    assert!(mid <= len);
+
+    (&mut slice[..mid], &mut slice[mid..])
+} */
+
+fn split_at_mut<T>(slice: &mut [T], mid: usize) -> (&mut [T], &mut [T]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+    
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
+// We can also have unsafe traits
+unsafe trait YouFool {
+    // Implement
+}
+
+unsafe impl YouFool for i32 {
+    // Implement
+}
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+trait Small {
+    fn bark(&self);
+}
+
+trait Big {
+    fn bark(&self);
+}
+
+struct Doggo;
+
+impl Small for Doggo {
+    fn bark(&self) {
+        println!("*bork bork*");
+    }
+}
+
+impl Big for Doggo {
+    fn bark(&self) {
+        println!("*BARK*");
+    }
+}
+
+impl Doggo {
+    fn bark(&self) {
+        println!("*bark bark*");
     }
 }
